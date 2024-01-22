4d8ffb00975fe5bd2457816273687c4b667c756f
Glyn Davies <glynwdavies@gmail.com>
2024-01-22 13:20:07 +0000

    Seeded the Companies table

--- RentFraudDetector.Data/RentFraudDetectorDbContext.cs
+++ RentFraudDetector.Data/RentFraudDetectorDbContext.cs
@@ -9,6 +9,20 @@ public class RentFraudDetectorDbContext : DbContext
     
     public RentFraudDetectorDbContext(DbContextOptions<RentFraudDetectorDbContext> options)
         : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Company>().HasData(
+            new Company
+            {
+                Id = 1,
+                Name = "Conway"
+            },
+            new Company {
+                Id = 2,
+                Name = "Countryside"
+            }
+        );
     }
 }
