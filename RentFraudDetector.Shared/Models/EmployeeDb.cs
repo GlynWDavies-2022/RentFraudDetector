886bdc661ba36ba1132b0802395a6add80b8fe1c
Glyn Davies <glynwdavies@gmail.com>
2024-02-09 17:17:09 +0000

    Added equality for EmployeeDb.

--- RentFraudDetector.Shared/Models/EmployeeDb.cs
+++ RentFraudDetector.Shared/Models/EmployeeDb.cs
@@ -2,7 +2,7 @@
 
 namespace RentFraudDetector.Shared.Models;
 
-public class EmployeeDb
+public class EmployeeDb : IEquatable<EmployeeDb>
 {
     [Key]
     public int Id { get; init; }
@@ -17,4 +17,27 @@ public class EmployeeDb
     public byte[]? RollNumber { get; init; }
     public byte[]? Key { get; init; }
     public byte[]? Vector { get; init; }
+
+    public bool Equals(EmployeeDb? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return CompanyDbId == other.CompanyDbId && Equals(StaffNumber, other.StaffNumber) &&
+               Equals(FirstName, other.FirstName) && Equals(Surname, other.Surname) &&
+               Equals(SortCode, other.SortCode) && Equals(AccountNumber, other.AccountNumber) &&
+               Equals(RollNumber, other.RollNumber);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((EmployeeDb)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(CompanyDbId, StaffNumber, FirstName, Surname, SortCode, AccountNumber, RollNumber);
+    }
 }
